4afa9d91802e71e855c93d73f6a8ff6519014679
Heng <heng_cheang0407@hotmail.com>
2018-07-17 11:33:33 -0400

    2.8.2

---
+++ bombking/Assets/Unused/carrot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carrot : MonoBehaviour {
+    public static float slowTimeScale = 1.2f;
+    public static float normalTimeScale = 10f;
+    public static float timeOut = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "kim")
+        {
+            other.GetComponent<bluemonster>();
+            bluemonster.movespeed -= 1.8f;
+            other.GetComponent<timer>();
+            timer.maxtime += 10;
+            timer.timeleft += 10;
+            Destroy(gameObject);
+            //bluemonster.movespeed += 1.2f;
+        }
+       
+    }
+    public static IEnumerator CountDown()
+    {
+        move.speed = slowTimeScale;
+        yield return new WaitForSeconds(timeOut);
+        move.speed = normalTimeScale;
+    }
+}
---
+++ bombking/Assets/Used/Scripts/camreascript.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class camreascript : MonoBehaviour
+
+{
+
+    //what we are following
+
+    public Transform target;
+
+    //zeros out the velocity
+
+    Vector3 velocity = Vector3.zero;
+
+    //time to follow target
+
+    public float smoothTime = .15f;
+
+    //enable and set maximum Y value
+
+    public bool YMaxEnabled = false;
+
+    public float YMaxValue = 0f;
+
+    //enable and set min Y value
+
+    public bool YMinEnabled = false;
+
+    public float YMinValue = 0f;
+
+    //enable and set X max vlaue
+
+    public bool XMaxEnabled = false;
+
+    public float XMaxValue = 0f;
+
+    //enable and set X min value
+
+    public bool XMinEnabled = false;
+
+    public float XMinValue = 0f;
+
+    void FixedUpdate()
+
+    {
+
+        //target position
+
+        Vector3 targetPos = target.position;
+
+        //vertical
+
+        if (YMinEnabled && YMaxEnabled)
+
+            //for the targets Y position we'll clamp that position between the Y min and the Y max
+
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, YMaxValue);
+
+        else if (YMinEnabled)
+
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, target.position.y);
+
+        else if (YMaxEnabled)
+
+            targetPos.y = Mathf.Clamp(target.position.y, target.position.y, YMaxValue);
+
+        //horizontal
+
+        if (XMinEnabled && XMaxEnabled)
+
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, XMaxValue);
+
+        else if (XMinEnabled)
+
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, targetPos.x);
+
+        else if (XMaxEnabled)
+
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, XMaxValue);
+
+        //align the camera and the targets z position
+
+        targetPos.z = transform.position.z;
+
+        //using smooth damp we will gradually change the camera transform position to the target
+
+        
+
+transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
+    }
+
+}
+
+
